ef9cc7b76812b9f293b327c3dbd8d4132eced6e3
maxw2 <mixcrtd@163.com>
2021-04-22 20:54:25 +0800

    手写Promise 基础类创建完毕 准备编写数据穿透

--- promise/promise.js
+++ promise/promise.js
@@ -1,1 +1,34 @@
+// 手写promise 异步函数
+const PENDING = 'pending'
+const FULFILLED = 'fulfilled'
+const REJECTED = 'rejected'
 
+
+class MyPromise {
+    status = PENDING
+    resolveCallBackArr = []
+    rejectCallBackArr = []
+    constructor(fn) {
+       
+    }
+
+    resolve(data) {
+        console.log(this)
+        status = 'fulfilled'
+        const cb = this.resolveCallBackArr.shift()
+        if(cb) cb(data)
+        
+    }
+
+    reject(data) {
+        status = 'rejected'
+    }
+
+    then(resolveFn, rejectFn) {
+        console.log(this)
+        this.resolveCallBackArr.push.bind(this, resolveFn)
+        this.rejectCallBackArr.push.bind(this, rejectFn)
+    }
+
+
+}
